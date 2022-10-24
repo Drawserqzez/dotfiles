0e9d4a4a1f7c28d730ed816f7d759b85feadf706
Drawserqzez <me@holton.lol>
2022-10-25 00:05:33 +0200

    Add config.fish file

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Nvm is cool
+nvm use lts
+clear
+
+# add rust directory to path
+fish_add_path ~/.cargo/bin/
