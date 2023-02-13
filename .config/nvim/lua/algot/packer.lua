2ef7ce84f0ed713831ee7882bc498fa2d9b880fc
Drawserqzez <me@holton.lol>
2023-02-13 21:49:08 +0100

    Add alpha-nvim dashboard

--- .config/nvim/lua/algot/packer.lua
+++ .config/nvim/lua/algot/packer.lua
@@ -64,4 +64,8 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons' }
     }
+
+    use {
+        'goolord/alpha-nvim',
+    }
 end)
