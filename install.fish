18d4aefdc191dd40298538d4ab449d0c1b89edc5
Drawserqzez <me@holton.lol>
2023-06-12 14:14:05 +0200

    Add install instruction, tweak install script

--- install.fish
+++ install.fish
@@ -15,6 +15,8 @@ git remote add origin https://github.com/Drawserqzez/dotfiles
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull origin main --force
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout main --force
 
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showuntrackedfiles no
+
 # Nvim specific setup
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
