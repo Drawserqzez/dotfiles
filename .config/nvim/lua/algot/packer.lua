b2a94a67bf12cba730a1bdc56698965664141cab
Drawserqzez <me@holton.lol>
2023-09-15 16:57:02 +0200

    re-add colourscheme

--- .config/nvim/lua/algot/packer.lua
+++ .config/nvim/lua/algot/packer.lua
@@ -73,4 +73,7 @@ return require('packer').startup(function(use)
         'folke/todo-comments.nvim',
         requires = 'nvim-lua/plenary.nvim'
     }
+
+    use { 'rebelot/kanagawa.nvim' }
+
 end)
