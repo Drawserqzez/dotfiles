cf46c81cfe4225d7aa2f24e4b4ae0c479ff41667
Drawserqzez <me@holton.lol>
2023-11-09 04:42:32 +0100

    Add startup line for starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,8 @@ fish_add_path ~/.cargo/bin/
 
 # fzf.fish keybinds
 fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs
+
+if starship --version >> /dev/null
+    starship init fish | source
+end
+
