424e2d5d413b7801877ada809e7748f809e54569
Drawserqzez <me@holton.lol>
2024-06-13 11:44:58 +0200

    Configure transient prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,12 @@ fish_add_path ~/.cargo/bin/
 # fzf.fish keybinds
 fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs
 
+function starship_transient_rprompt_func
+    starship module time
+end
+
 if starship --version >> /dev/null
     starship init fish | source
+    enable_transience
 end
 
