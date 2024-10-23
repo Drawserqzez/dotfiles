9819b7892c8e7c2522dcb7d31d65461668ca6c14
Drawserqzez <me@holton.lol>
2024-10-23 10:11:28 +0200

    Add keybinds for neotest

--- .config/nvim/lua/algot/config/neotest.lua
+++ .config/nvim/lua/algot/config/neotest.lua
@@ -1,5 +1,22 @@
 local neotest = require('neotest')
 local dotnet = require('neotest-dotnet')
+local dap = require('dap')
+
+local run_with_dap = function()
+    neotest.summary.open()
+    neotest.run.run({ strategy="dap" })
+    dap.repl.open()
+end
+
+local rerun_with_dap = function()
+    neotest.summary.open()
+    neotest.run.run_lastt({ strategy="dap" })
+    dap.repl.open()
+end
+
+local open_summary = function()
+    neotest.summary.open()
+end
 
 neotest.setup({
     adapters = {
@@ -12,8 +29,11 @@ neotest.setup({
     }
 })
 
-vim.keymap.set('n', '<leader>trr', function() neotest.run.run() end)
-vim.keymap.set('n', '<leader>trs', function() neotest.run.stop() end)
-vim.keymap.set('n', '<leader>trd', function() neotest.run.run({ strategy="dap" }) end)
--- todo: add more keymaps to open e.x. summary or output
+vim.keymap.set('n', '<leader>trr', function() open_summary() neotest.run.run() end)
+vim.keymap.set('n', '<leader>trf', function() open_summary() neotest.run.run(vim.fn.expand('%')) end)
+vim.keymap.set('n', '<leader>tra', function() open_summary() neotest.run.run({suite=true}) end)
+vim.keymap.set('n', '<leader>trd', function() run_with_dap() end)
+vim.keymap.set('n', '<leader>trw', function() rerun_with_dap() end)
+
+vim.keymap.set('n', '<leader>trs', function() open_summary() neotest.run.stop() end)
 
