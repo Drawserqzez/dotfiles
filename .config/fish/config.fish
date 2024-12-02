0cadd805d0ebcfb89704095db863730362bc103a
Drawserqzez <me@holton.lol>
2024-12-02 17:36:11 +0100

    Change from starship prompt to tide instead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,32 +5,28 @@ end
 fish_vi_key_bindings
 
 # Nvm is cool
-nvm use lts -s
+if type -q nvm 
+    nvm use lts -s
+end
 
 # add rust directory to path
 fish_add_path ~/.cargo/bin/
 
 # fzf.fish keybinds
-fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs --processes=\cp
+if type -q fisher
+    fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs --processes=\cp
+end
 
 function starship_transient_rprompt_func
     starship module time
 end
 
-if type -q starship
-    starship init fish | source
-    enable_transience
+function starship_transient_prompt_func
+    starship module character
 end
 
-
-set -gx LANG "en_US.UTF-8"
-set -gx LC_NUMERIC "sv_SE.UTF-8"
-set -gx LC_TIME "sv_SE.UTF-8"
-set -gx LC_MONETARY "sv_SE.UTF-8"
-set -gx LC_PAPER "sv_SE.UTF-8"
-set -gx LC_NAME "sv_SE.UTF-8"
-set -gx LC_ADDRESS "sv_SE.UTF-8"
-set -gx LC_TELEPHONE "sv_SE.UTF-8"
-set -gx LC_MEASUREMENT "sv_SE.UTF-8"
-set -gx LC_IDENTIFICATION "sv_SE.UTF-8"
+if type -q starship
+    starship init fish | source
+    # enable_transience
+end
 
