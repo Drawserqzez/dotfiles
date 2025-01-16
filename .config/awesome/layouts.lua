53620b5d905b9b7fbee99fef286bc48d110d7862
Drawserqzez <me@holton.lol>
2025-01-16 16:49:38 +0100

    Add missing layouts file

---
+++ .config/awesome/layouts.lua
@@ -0,0 +1,11 @@
+local awful = require('awful')
+
+local layouts = {
+    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    awful.layout.suit.spiral,
+    awful.layout.suit.max,
+    awful.layout.suit.fullscreen
+}
+
+return layouts
