f01db333efe9198a4ca8224b6d39e6b84988b7d3
Drawserqzez <me@holton.lol>
2025-01-17 12:20:34 +0100

    Add better defaults for tags

--- .config/awesome/tags.lua
+++ .config/awesome/tags.lua
@@ -4,9 +4,9 @@ local tags = {
     { tag = "一", layout = layouts[3] },
     { tag = "二", layout = layouts[3] },
     { tag = "三", layout = layouts[3] },
-    { tag = "四", layout = layouts[1] },
-    { tag = "五", layout = layouts[1] },
-    { tag = "六", layout = layouts[1] },
+    { tag = "四", layout = layouts[2] },
+    { tag = "五", layout = layouts[2] },
+    { tag = "六", layout = layouts[2] },
     { tag = "七", layout = layouts[1] },
     { tag = "八", layout = layouts[1] },
     { tag = "九", layout = layouts[1] }
