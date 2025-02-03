5b25ecf4772e73ad4163ecd4d4e5eeec4d660d11
Drawserqzez <me@holton.lol>
2025-02-03 16:19:47 +0100

    Finally fix the stupid battery widget :D

--- .config/awesome/widgets/battery.lua
+++ .config/awesome/widgets/battery.lua
@@ -1,7 +1,6 @@
 local awful = require('awful')
 local wibox = require('wibox')
 local gears = require('gears')
-local watch = require('awful.widget.watch')
 
 local clickable = require('widgets.clickable-container')
 
@@ -10,18 +9,21 @@ local text_widget = wibox.widget {
     font = 'GeistMono NF Regular 8',
     ellipsize = 'none',
     id = 'textboxw',
+    text = 'g'
+}
+
+local widget_with_text = wibox.widget {
+    text_widget,
+    widget = wibox.container.place,
+    fill_vertical = true,
+    fill_horizontal = true,
+    valign = 'center',
+    halign = 'center',
 }
 
 local progress_widget = wibox.widget {
-    wibox.widget {
-        text_widget,
-        widget = wibox.container.place,
-        fill_vertical = true,
-        fill_horizontal = true,
-        valign = 'center',
-        halign = 'center',
-    },
     widget = wibox.container.arcchart,
+    widget_with_text,
     min_value = 0,
     max_value = 1,
     value = 0,
@@ -30,23 +32,22 @@ local progress_widget = wibox.widget {
     forced_height = 24,
     forced_width = 24,
     rounded_edge = true,
-    colors = { '#ffffff', '#ffffff', },
+    colors = { '#ffffee', '#00ff00' },
     paddings = 2,
 }
 
 local battery_widget = clickable(progress_widget)
 
-Charge = ''
+local charge
 
-local function update_battery_status(battery_status)
+local function update_battery_status(widget, battery_status)
+    print(battery_status.charge_level)
+    print(battery_status.is_charging)
     local charge_percentage = battery_status.charge_level / 100
-    require('naughty').notify({ text = battery_status.charge_level })
-    progress_widget.values = { 1.0 - (charge_percentage or 0.0), charge_percentage or 0.0 }
 
-    -- local text = battery_status.charge_level .. ' %'
-    --
-    local text = 'd'
+    widget.values = { 1.0 - (charge_percentage or 0.0), charge_percentage or 0.0 }
 
+    local text = ''
     if battery_status.is_charging then
         text = '󱐋'
     end
@@ -54,32 +55,32 @@ local function update_battery_status(battery_status)
     text_widget:set_text(text)
 end
 
--- local battery_timer
--- progress_widget, battery_timer = watch('acpi', 1,
-watch('acpi', 1,
-    function(callback_widget, stdout, _, _, _)
-        Charge = stdout
+awful.widget.watch('acpi', 60,
+    function(callback_widget, stdout)
+        charge = stdout
 
-        if stdout:find("Battery") then
-            -- Pattern to match the primary battery information
-            local battery_info = stdout:match("Battery 0: (%a+), (%d+)%")
-
-            Charge = Charge .. 'battery_info'
-            if battery_info then
-                local status, charge_level = battery_info:match("(%a+), (%d+)")
-
-                update_battery_status({
-                    is_charging = status == 'Charging' or status == 'Full',
-                    charge_level = charge_level
-                })
-            end
-        else
-            -- we don't have a battery, so no use in running the watch
-            -- progress_widget.visible = false
-            -- battery_timer:stop()
-            Charge = Charge .. ' not working :(('
+        -- Split the output into lines
+        local lines = {}
+        for line in stdout:gmatch("[^\r\n]+") do
+            table.insert(lines, line)
         end
 
+        -- Assuming the first line contains the battery information
+        local battery_info = lines[1]
+
+        -- Parse the battery information
+        local status, percentage = battery_info:match("(%a+), (%d+%%)")
+
+        if status and percentage then
+            print(percentage)
+            local is_charging = (status:find("Charging") ~= nil)
+            update_battery_status(callback_widget, { is_charging = is_charging, charge_level = tonumber(percentage:sub(1, -2)) })
+
+            charge = percentage
+            if is_charging then
+                charge = charge .. ' and charging'
+            end
+        end
     end,
     progress_widget
 )
@@ -88,7 +89,7 @@ battery_widget:buttons(
     gears.table.join(
         awful.button({}, 1, nil,
             function()
-                require('naughty').notify({ title = 'Battery Status', text = Charge })
+                require('naughty').notify({ title = 'Battery Status', text = charge })
             end
         )
     )
