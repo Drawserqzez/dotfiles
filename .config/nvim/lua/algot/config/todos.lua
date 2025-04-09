5319b6085874ff6f37ffb15c549bd9190ec11060
Drawserqzez <me@holton.lol>
2025-04-09 17:40:06 +0200

    Updated todo config

--- .config/nvim/lua/algot/config/todos.lua
+++ .config/nvim/lua/algot/config/todos.lua
@@ -3,16 +3,25 @@ local todo = require('todo-comments')
 todo.setup {
     keywords = {
         TODO = {
+            icon = " ",
+            color = "info",
             alt = { "todo", "ToDo", "Todo" }
         },
-        FIXME = {
-            alt = { "fixme", "FixMe", "Fixme" }
-        } -- todo: more keywords?
-    }
+        FIX = {
+            icon = " ",
+            color = "error",
+            alt = { "fixme", "FixMe", "Fixme", "bug", "BUG", "Bug" }
+        },
+        HACK = {
+            alt = { "hack", "Hack" }
+        },
+        -- todo: more keywords?
+    },
+    merge_keywords = true
 }
 
 local config = require('todo-comments.config')
 
-vim.keymap.set('n', '<leader>td', vim.cmd.TodoTrouble)
-vim.keymap.set('n', '<leader>ts', vim.cmd.TodoTelescope)
+-- vim.keymap.set('n', '<leader>td', vim.cmd.TodoTrouble)
+vim.keymap.set('n', '<leader>td', vim.cmd.TodoTelescope)
 
